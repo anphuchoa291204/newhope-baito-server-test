5c9a316ad1fbfb58e402e167fd32e46c67bb0cf7
anphuchoa291204 <142864120+hoapooh@users.noreply.github.com>
2024-12-07 00:51:25 +0700

    change temporarily to app.js

--- server/app.js
+++ server/app.js
@@ -67,4 +67,14 @@ app.use(globalErrorHandler)
 
 // CHECKPOINT: 7. Export the app for running the server
 
-export default app
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+	console.log("Server is running on port 8080")
+})
+
+// CHECKPOINT: UNHANDLED REJECTION
+process.on("unhandledRejection", (err) => {
+	console.log(err.name, err.message)
+	console.log("UNHANDLED REJECTION! Shutting down...")
+	process.exit(1)
+})
